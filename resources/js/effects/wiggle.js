af3479b2c56a15adafb30a81e7ece2adda239e48
Michelle Huh <michelle.huh@hotmail.com>
2018-05-26 06:39:02 -0700

    contact section and related effects added.

--- resources/js/effects/wiggle.js
+++ resources/js/effects/wiggle.js
@@ -1,8 +1,8 @@
 document.addEventListener("scroll", function() {
-    $('.slide-in').each(function(){
-        var elPosition  = $(this).offset().top;
-        var elHeight    = $(this).height();
-        var windowTop   = $(window).scrollTop();
+    $('.wiggle').each(function(){
+        var elPosition = $(this).offset().top;
+        var elHeight = $(this).height();
+        var windowTop = $(window).scrollTop();
         var windowHeight = $(window).height();
 
         if (elPosition < windowTop + windowHeight - elHeight) {
